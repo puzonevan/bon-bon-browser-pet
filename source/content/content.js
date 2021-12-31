92d2d8d4cbfda3b7fb11929877465cd5356b481e
‘Evan <puzonevan@gmail.com>
2021-12-31 16:23:01 -0500

    bon bon in every webpage

--- source/content/content.js
+++ source/content/content.js
@@ -1,13 +1,18 @@
-console.log("Hello Worlds");
+
+const create
 
 const bonbon = document.createElement("img");
 bonbon.src = "chrome-extension://bpipdiepaajdcmdghpnpecihdigijpmg/source/content/shark.gif";
 bonbon.alt = "bon bon browser";
 bonbon.style.height = "100px";
 bonbon.style.width = "100px";
+bonbon.style.zIndex = "100";
+bonbon.style.position = "absolute";
+bonbon.style.right = "0";
+bonbon.style.bottom = "0";
 
 const hello = document.createElement("h1");
 hello.innerHTML = "Hello";
-hello.style.zIndex = "100";
+hello.style.zIndex = "10000";
 
 (document.head || document.documentElement).appendChild(bonbon);
